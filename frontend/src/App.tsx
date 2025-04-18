30981096d9a9b14b704d9b04c969657306c2f875
PavloProkopenko <stpasha6@gmail.com>
2025-04-18 16:04:08 +0300

    first changes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Center, Heading } from "@chakra-ui/react"
+
+function App() {
+  return(
+    <Center>
+      <Heading as='h2' size='xl' fontWeight={600}>
+        Printing Management
+      </Heading>
+    </Center>
+  )
+}
+
+export default App
